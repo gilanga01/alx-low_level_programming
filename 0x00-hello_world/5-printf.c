3875d400b8cbf69f8bb461f502fd587520a405cc
Langat <langatgilbert2013@gmail.com>
2023-09-08 02:01:07 -0700

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-i#include <stdio.h>
+#include <stdio.h>
 /**
- * main - main block
- * Return: 0
+ * main - a program that uses printf not puts
+ * Return:0(Success);
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
